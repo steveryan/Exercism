d9fdfca9ff53bdea67ef8fefe7bcdb199774e643
steveryan <steveryan6989@gmail.com>
2019-04-22 22:52:38 -0400

    Refactor pangram to use .all? method

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,16 +1,7 @@
 class Pangram
   ALPHABET = ("a".."z").to_a
   def self.pangram?(sentence)
-    value = true
-    ALPHABET.each do |letter|
-      if sentence.downcase.include? letter
-        value = true
-      else 
-        value = false
-        return value
-      end
-    end
-    return value
+    ALPHABET.all? { |letter| sentence.downcase.include? letter}
   end
 end
 
