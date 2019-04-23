a91c32cfd59cb754cfef42cfc37ac2b00193da4a
Steve Ryan <steveryan6989@gmail.com>
2019-04-23 11:20:28 -0400

    finished the high scores exercise

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,5 +1,22 @@
 class HighScores
-  def self.method
+  def initialize(score)
+    @scores = score
+  end
+  
+  def scores
+   @scores
+  end
+
+  def personal_top_three
+    @scores.sort.reverse[0..2]
+  end
+  
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
   end
 end
 
