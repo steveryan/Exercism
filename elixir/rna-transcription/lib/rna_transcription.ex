a1d4f00365e655cb44a09b0ab925cf654f099dbd
Steve Ryan <steveryan6989@gmail.com>
2020-02-28 11:34:07 -0500

    Rna transcription working, but I'm unhappy with it

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -7,7 +7,18 @@ defmodule RnaTranscription do
   iex> RnaTranscription.to_rna('ACTG')
   'UGAC'
   """
+  
+  def translate(letter) do
+    case letter do
+      67 -> List.first('G')
+      71 -> List.first('C')
+      84 -> List.first('A')
+      65 -> List.first('U')
+    end
+  end
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    Enum.map(dna, fn x -> translate(x) end)
   end
+
 end
