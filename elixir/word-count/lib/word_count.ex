7257c2b075785f1dc24c96a355b7807d57e7dd22
steveryan <steveryan6989@gmail.com>
2020-03-05 20:39:56 -0500

    formatted

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,16 +6,18 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    clean_sentence = String.replace(sentence, "_", " ")
-      |>String.replace(","," ")
-      |>String.replace("!"," ")
-      |>String.replace("&"," ")
-      |>String.replace("@"," ")
-      |>String.replace("$"," ")
-      |>String.replace("%"," ")
-      |>String.replace("^"," ")
-      |>String.replace("&"," ")
-      |>String.replace(":"," ")
+    clean_sentence =
+      String.replace(sentence, "_", " ")
+      |> String.replace(",", " ")
+      |> String.replace("!", " ")
+      |> String.replace("&", " ")
+      |> String.replace("@", " ")
+      |> String.replace("$", " ")
+      |> String.replace("%", " ")
+      |> String.replace("^", " ")
+      |> String.replace("&", " ")
+      |> String.replace(":", " ")
+
     word_list = ~w(#{clean_sentence})
     Enum.frequencies_by(word_list, &String.downcase/1)
   end
