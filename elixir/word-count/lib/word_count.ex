84eab46d371fb67c94421d1db5a9bed53fa1650d
Steve Ryan <steveryan6989@gmail.com>
2020-03-05 19:07:38 -0500

    Solved word count, unhappy I couldn't get the regex working

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,17 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    clean_sentence = String.replace(sentence, "_", " ")
+      |>String.replace(","," ")
+      |>String.replace("!"," ")
+      |>String.replace("&"," ")
+      |>String.replace("@"," ")
+      |>String.replace("$"," ")
+      |>String.replace("%"," ")
+      |>String.replace("^"," ")
+      |>String.replace("&"," ")
+      |>String.replace(":"," ")
+    word_list = ~w(#{clean_sentence})
+    Enum.frequencies_by(word_list, &String.downcase/1)
   end
 end
